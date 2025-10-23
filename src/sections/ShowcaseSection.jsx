fde3e54a9f71fdd1c8ac36b3c6f5776df6ce4e30
Malith Anjana <malithanjana97@gmail.com>
2025-10-23 14:14:45 +0530

    add projects to showcase

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
+import TitleHeader from "../components/TitleHeader.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -46,39 +47,91 @@ const AppShowcase = () => {
   return (
     <div id="work" ref={sectionRef} className="app-showcase">
       <div className="w-full">
-        <div className="showcaselayout">
-          <div ref={rydeRef} className="first-project-wrapper">
-            <div className="image-wrapper">
-              <img src="/images/project1.png" alt="Ryde App Interface" />
+        <TitleHeader
+            title="Featured Projects"
+            sub="📋 My Work Showcase"
+        />
+        <div className="showcaselayout mt-22">
+          <div ref={rydeRef} className="first-project-wrapper card-border rounded-xl p-8">
+              <div className="image-wrapper">
+              <img src="/images/project1.png" className="object-fit" alt="Ryde App Interface"/>
             </div>
             <div className="text-content">
               <h2>
-                On-Demand Rides Made Simple with a Powerful, User-Friendly App
-                called Ryde
+                Empowering Organizations to Prevent Phishing Attacks
               </h2>
               <p className="text-white-50 md:text-xl">
-                An app built with React Native, Expo, & TailwindCSS for a fast,
-                user-friendly experience.
+                A web application designed to help organizations prevent phishing attacks by simulating real-world phishing scenarios.
               </p>
+              <div className="flex flex-row gap-4">
+                <a href="#" className="action-btn group">
+                  <div className="inner">
+                    <img src="/images/github.png" alt="github Logo" className="w-6 h-6 object-fit"/>
+                    <span> Github</span>
+                  </div>
+                </a>
+                <a href="#" className="action-btn group">
+                  <div className="inner">
+                    <img src="/images/live.png" alt="github Logo" className="w-6 h-6 object-fit"/>
+                    <span>Live Demo</span>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
 
           <div className="project-list-wrapper overflow-hidden">
-            <div className="project" ref={libraryRef}>
+            <div className="project card-border rounded-xl p-8" ref={libraryRef}>
               <div className="image-wrapper bg-[#FFEFDB]">
                 <img
-                  src="/images/project2.png"
-                  alt="Library Management Platform"
+                    src="/images/project2.png"
+                    className="object-fill"
+                    alt="Library Management Platform"
                 />
               </div>
-              <h2>The Library Management Platform</h2>
+              <h2>Designed to Bring Gamers Together</h2>
+              <p className="text-white-50 md:text-md mt-2">
+                An online hub where players can explore games, track progress, and connect with others.
+              </p>
+              <div className="flex flex-row gap-4 mt-4">
+                <a href="#" className="action-btn group">
+                  <div className="inner">
+                    <img src="/images/github.png" alt="github Logo" className="w-6 h-6 object-fit"/>
+                    <span> Github</span>
+                  </div>
+                </a>
+                <a href="#" className="action-btn group">
+                  <div className="inner">
+                    <img src="/images/live.png" alt="github Logo" className="w-6 h-6 object-fit"/>
+                    <span>Live Demo</span>
+                  </div>
+                </a>
+              </div>
             </div>
 
-            <div className="project" ref={ycDirectoryRef}>
+            <div className="project card-border rounded-xl p-8" ref={ycDirectoryRef}>
               <div className="image-wrapper bg-[#FFE7EB]">
-                <img src="/images/project3.png" alt="YC Directory App" />
+                <img src="/images/project3.png" alt="YC Directory App"/>
               </div>
               <h2>YC Directory - A Startup Showcase App</h2>
+              <p className="text-white-50 md:text-md mt-2">
+                A web application designed to help organizations prevent phishing attacks by simulating real-world
+                phishing scenarios.
+              </p>
+              <div className="flex flex-row gap-4 mt-4">
+                <a href="#" className="action-btn group">
+                  <div className="inner">
+                    <img src="/images/github.png" alt="github Logo" className="w-6 h-6 object-fit"/>
+                    <span> Github</span>
+                  </div>
+                </a>
+                <a href="#" className="action-btn group">
+                  <div className="inner">
+                    <img src="/images/live.png" alt="github Logo" className="w-6 h-6 object-fit"/>
+                    <span>Live Demo</span>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
