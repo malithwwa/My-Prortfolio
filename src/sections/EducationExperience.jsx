ca38148b4ddc13dd82ce61f5f26c0828e766e80f
Malith Anjana <malithanjana97@gmail.com>
2025-10-23 22:42:16 +0530

    update the timeline

--- src/sections/EducationExperience.jsx
+++ src/sections/EducationExperience.jsx
@@ -16,9 +16,9 @@ const EducationExperience = () => {
                 <div className="my-20">
                     <div class="grid grid-cols-3 lg:grid-cols-5 gap-5 lg:gap-20">
 
-                        <section class="col-span-3">
-                            <header class="pb-5 ml-16">
-                                <h2 class="text-2xl font-semibold tracking-wide">EXPERIENCE</h2>
+                        <section className="col-span-3">
+                            <header className="pb-5 ml-16">
+                                <h2 className="text-2xl font-semibold tracking-wide">EXPERIENCE</h2>
                             </header>
 
                             {/*Card Wrapper*/}
@@ -114,47 +114,62 @@ const EducationExperience = () => {
                         </section>
 
 
-                        <section class="col-span-2">
-                            <header class="pb-5 pl-8">
-                                <h2 class="text-2xl font-semibold tracking-wide">EXPERIENCE</h2>
+                        <section className="col-span-2">
+                            <header className="pb-5 ml-16">
+                                <h2 className="text-2xl font-semibold tracking-wide">EDUCATION</h2>
                             </header>
 
-                            <div class="border-l-2 border-[#bababa] pl-8">
-
+                            {/*Card Wrapper*/}
+                            <div className="card-border relative rounded-xl p-3 flex flex-col gap-4 ml-16">
+                                {/*Timeline*/}
+                                <span
+                                    className="absolute w-8 h-8 rounded-full bg-[#00ff55] border-5 border-white -left-16 top-14 z-60"/>
                                 <div
-                                    class="relative bg-white border border-[#eaeaea] p-5 mb-5 cursor-pointer transition-all duration-300 hover:shadow-md hover:border-transparent">
-                                    <span
-                                        class="absolute w-4 h-4 rounded-full bg-[#11a372] border-2 border-white -left-5 top-5"></span>
-                                    <h4 class="text-[#11a372] font-semibold">2018 - 2022</h4>
-                                    <h3 class="text-[19px] font-helvetica text-[#444] py-2">UI/UX Designer</h3>
-                                    <p class="text-[#666] text-[17px] leading-tight">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna aliqua.
-                                    </p>
+                                    className="absolute -left-11 top-25 h-[108%] w-[4px] -translate-x-1 bg-[#00ff55] z-50"/>
+                                {/*Content*/}
+                                <div className="exp-card-wrapper ml-10 my-10 pe-6">
+                                    <div className="flex items-start">
+                                        <div className="expText flex xl:gap-20 md:gap-2 gap-5 relative z-20">
+                                            <div>
+                                                <h1 className="font-semibold text-xl">BSc. (Hons) Degree in Information
+                                                    Technology
+                                                    Specialization in Software Engineering</h1>
+                                                <p className="my-5 text-white-50">
+                                                    🗓️&nbsp;Feb 2019 - March 2023
+                                                </p>
+                                                <p className="text-[#839CB5]">
+                                                    Sri Lanka Institute of Information Technology (SLIIT)
+                                                </p>
+
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
+                            </div>
 
+                            {/*Card Wrapper*/}
+                            <div className="card-border relative rounded-xl p-3 flex flex-col gap-4 mt-20 ml-16">
+                                {/*Timeline*/}
+                                <span
+                                    className="absolute w-8 h-8 rounded-full bg-[#00ff55] border-5 border-white -left-16 top-14 z-60"/>
                                 <div
-                                    class="relative bg-white border border-[#eaeaea] p-5 mb-5 cursor-pointer transition-all duration-300 hover:shadow-md hover:border-transparent">
-                                    <span
-                                        class="absolute w-4 h-4 rounded-full bg-[#11a372] border-2 border-white -left-5 top-5"></span>
-                                    <h4 class="text-[#11a372] font-semibold">2018 - 2022</h4>
-                                    <h3 class="text-[19px] font-helvetica text-[#444] py-2">Full-Stack Developer</h3>
-                                    <p class="text-[#666] text-[17px] leading-tight">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna aliqua.
-                                    </p>
-                                </div>
+                                    className="absolute -left-11 top-25 h-[55%] w-[4px] -translate-x-1 bg-[#00ff55] z-50"/>
+                                {/*Content*/}
+                                <div className="exp-card-wrapper ml-10 my-10 pe-6">
+                                    <div className="flex items-start">
+                                        <div className="expText flex xl:gap-20 md:gap-2 gap-5 relative z-20">
+                                            <div>
+                                                <h1 className="font-semibold text-xl">Diploma in IT (International</h1>
+                                                <p className="my-5 text-white-50">
+                                                    🗓️&nbsp; September 2018 - January 2019
+                                                </p>
+                                                <p className="text-[#839CB5]">
+                                                    ESOFT Metro Campus
+                                                </p>
 
-                                <div
-                                    class="relative bg-white border border-[#eaeaea] p-5 mb-5 cursor-pointer transition-all duration-300 hover:shadow-md hover:border-transparent">
-                                    <span
-                                        class="absolute w-4 h-4 rounded-full bg-[#11a372] border-2 border-white -left-5 top-5"></span>
-                                    <h4 class="text-[#11a372] font-semibold">2018 - 2022</h4>
-                                    <h3 class="text-[19px] font-helvetica text-[#444] py-2">Frontend Developer</h3>
-                                    <p class="text-[#666] text-[17px] leading-tight">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna aliqua.
-                                    </p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </section>
@@ -163,6 +178,6 @@ const EducationExperience = () => {
 
             </div>
         </section>
-)
+    )
 }
 export default EducationExperience
